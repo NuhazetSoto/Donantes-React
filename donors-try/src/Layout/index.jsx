a4a7a8a899eaea08f50b0b2e10488d4a60f4aa05
Ramses Dominguez Ferrera <rdominguezferrera@gmail.com>
2023-06-15 14:36:13 +0100

    footer erased

--- donors-try/src/Layout/index.jsx
+++ donors-try/src/Layout/index.jsx
@@ -3,7 +3,6 @@ import Header from '../Components/Header/Header'
 import Footer from '../Components/Footer/Footer'
 import { Outlet } from 'react-router-dom'
 import './index.css'
-import Footer from '../Components/Footer/Footer'
 
 function Root() {
 
