7891617452a4d6db18d89fc00b7c42844f96f1cd
josecos <jocaorsa@gmail.com>
2023-06-20 12:37:17 +0100

    subida y cambios en donantes

---
+++ donors-try/src/Components/FechaCita/FechaHoraCita.jsx
@@ -0,0 +1,40 @@
+import * as React from 'react'
+import Tooltip from '@mui/material/Tooltip'
+import Stack from '@mui/material/Stack'
+import { DemoContainer, DemoItem } from '@mui/x-date-pickers/internals/demo'
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider'
+import { DateTimePicker } from '@mui/x-date-pickers/DateTimePicker'
+import { CardContent, InputLabel } from '@mui/material'
+
+
+function Label({ componentName, valueType, isProOnly }) {
+  const content = (
+    <span>
+      <strong>{componentName}</strong> for {valueType} editing
+    </span>
+  )
+
+  if (isProOnly) {
+    return (
+      <Stack direction="row" spacing={0.5} component="span">
+        <Tooltip title="Included on Pro package">
+          <a href="/x/introduction/licensing/#pro-plan">
+            <span className="plan-pro" />
+          </a>
+        </Tooltip>
+        {content}
+      </Stack>
+    )
+  }
+
+  return content
+}
+
+export default function CommonlyUsedComponents() {
+  return (
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+          <DateTimePicker />
+    </LocalizationProvider>
+  )
+}
