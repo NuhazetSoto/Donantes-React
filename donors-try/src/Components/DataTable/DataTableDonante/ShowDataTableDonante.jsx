623a78262144092c24aa7c9ddd0a8e74dd65f806
Nuhazet <nuhazet1997@gmail.com>
2023-06-20 13:56:14 +0100

    Merge, Edit Usuarios por Admin, continuo con los puntos

--- donors-try/src/Components/DataTable/DataTableDonante/ShowDataTableDonante.jsx
+++ donors-try/src/Components/DataTable/DataTableDonante/ShowDataTableDonante.jsx
@@ -10,7 +10,7 @@ import {
 import { useState, useEffect } from 'react'
 import { getOneDonante } from '../../../services/donante.service'
 import { Link } from 'react-router-dom'
-import Search from '../../Search/Search'
+import TableSearch from '../../Search/TableSearch'
 
 const columns = [
   { field: 'dni', headerName: 'DNI', width: 70 },
