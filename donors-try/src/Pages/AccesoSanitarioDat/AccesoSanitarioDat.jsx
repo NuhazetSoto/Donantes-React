ba6bb9d4ac62f582800e9adc62b482a6c0a01317
Ramses Dominguez Ferrera <rdominguezferrera@gmail.com>
2023-06-23 09:46:09 +0100

    merge

--- donors-try/src/Pages/AccesoSanitarioDat/AccesoSanitarioDat.jsx
+++ donors-try/src/Pages/AccesoSanitarioDat/AccesoSanitarioDat.jsx
@@ -92,21 +92,11 @@ function AccesoSanitarioDat() {
           }}
           variant="contained"
           color="error"
-<<<<<<< HEAD
-          onClick={() => {
-            localStorage.removeItem('token')
-            localStorage.removeItem('id')
-            localStorage.removeItem('role')
-          }}
-        >
-          <Link style={{ color: 'inherit', textDecoration: 'none' }} to={'/'}>
-=======
         >
           <Link
             style={{ color: 'inherit', textDecoration: 'none' }}
             to={'/'}
           >
->>>>>>> 8d9bb2363e60a4ed6d337984feadfb41440bfb0b
             Cerrar sesión
           </Link>
         </Button>
@@ -159,58 +149,6 @@ function AccesoSanitarioDat() {
                 justifyContent: 'center',
                 alignItems: 'center',
                 width: '100%',
-<<<<<<< HEAD
-                marginLeft: '80px',
-              }}
-            >
-              <ListItem sx={{ fontWeight: 'bold' }}>
-                -Nombre:
-                <Typography sx={{ marginLeft: '5px' }}>{data.name}</Typography>
-              </ListItem>
-
-              <ListItem sx={{ fontWeight: 'bold' }}>
-                -Apellidos:
-                <Typography sx={{ marginLeft: '5px' }}>
-                  {data.lastname}
-                </Typography>
-              </ListItem>
-
-              <ListItem sx={{ fontWeight: 'bold' }}>
-                -DNI:
-                <Typography sx={{ marginLeft: '5px' }}>{data.dni}</Typography>
-              </ListItem>
-
-              <ListItem sx={{ fontWeight: 'bold' }}>
-                -Email:
-                <Typography sx={{ marginLeft: '5px' }}>{data.email}</Typography>
-              </ListItem>
-
-              <ListItem sx={{ fontWeight: 'bold' }}>
-                -TLF:
-                <Typography sx={{ marginLeft: '5px' }}>{data.phone}</Typography>
-              </ListItem>
-
-              <ListItem sx={{ fontWeight: 'bold' }}>
-                -Fecha de nacimiento:
-                <Typography sx={{ marginLeft: '5px' }}>
-                  {data.fecha_nacimiento}
-                </Typography>
-              </ListItem>
-
-              <ListItem sx={{ fontWeight: 'bold' }}>
-                -Localidad:
-                <Typography sx={{ marginLeft: '5px' }}>
-                  {data.location}
-                </Typography>
-              </ListItem>
-
-              <ListItem sx={{ fontWeight: 'bold' }}>
-                -Dirección:
-                <Typography sx={{ marginLeft: '5px' }}>
-                  {data.direction}
-                </Typography>
-              </ListItem>
-=======
               }}
             >
               <ListItem>-nombre:{data.name}</ListItem>
@@ -221,7 +159,6 @@ function AccesoSanitarioDat() {
               <ListItem>-Fecha de nacimiento:{data.fecha_nacimiento}</ListItem>
               <ListItem>-Localidad:{data.localidad}</ListItem>
               <ListItem>-Dirección:{data.direccion}</ListItem>
->>>>>>> 8d9bb2363e60a4ed6d337984feadfb41440bfb0b
             </List>
           </CardContent>
         </Card>
@@ -233,9 +170,6 @@ function AccesoSanitarioDat() {
             marginLeft: '100px'
           }}
         >
-<<<<<<< HEAD
-          <BasicModal getData={ getData } />
-=======
           <Link
             style={{ color: 'inherit', textDecoration: 'none' }}
             to={'/login/sanitario/misdatos/actualizar'}
@@ -256,7 +190,6 @@ function AccesoSanitarioDat() {
               Actualizar datos
             </Button>
           </Link>
->>>>>>> 8d9bb2363e60a4ed6d337984feadfb41440bfb0b
         </Box>
       </div>
     </>
